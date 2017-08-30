b3b090587a4afd7dd51a65564c327174e28eebad
mikemey <miguel.maestro@gmx.co.uk>
2017-08-30 10:26:54 +0100

    refactored common prices test code

--- backend-test/prices.spec.js
+++ backend-test/prices.spec.js
@@ -14,6 +14,7 @@ describe('GET /prices endpoint', () => {
   let app, server
 
   const getPriceData = () => request(app).get('/prices')
+  const coinfloorDelayMs = 50
 
   before(() => helpers.startTestServer((_app, _server) => {
     app = _app
@@ -33,6 +34,7 @@ describe('GET /prices endpoint', () => {
 
     nock('https://webapi.coinfloor.co.uk:8090')
       .get('/bist/XBT/GBP/ticker/')
+      .delay(coinfloorDelayMs)
       .reply(200, coinfloorResponse)
 
     done()
@@ -40,48 +42,39 @@ describe('GET /prices endpoint', () => {
 
   afterEach(() => nock.cleanAll())
 
+  const expectTicker = (actualJson, { ticker, buy, sell }, durMin = 1, durMax = 100) => {
+    const duration = actualJson.duration
+    duration.should.be.within(durMin, durMax)
+    actualJson.should.deep.equal({ ticker, buy, sell, duration })
+  }
+
   describe('SOLIDI page', () => {
     it('response with 200 and solidi prices', () => getPriceData()
       .expect(200)
-      .then(({ body }) => {
-        const duration = body[0].duration
-        duration.should.be.within(1, 100)
-        body.should.deep.contain({
-          ticker: 'solidi',
-          buy: '3626',
-          sell: '3448',
-          duration
-        })
-      }))
+      .then(({ body }) => expectTicker(body[0], {
+        ticker: 'solidi',
+        buy: '3626',
+        sell: '3448'
+      })))
   })
 
   describe('LakeBTC api', () => {
     it('response with 200 and lakebtc prices', () => getPriceData()
       .expect(200)
-      .then(({ body }) => {
-        const duration = body[1].duration
-        duration.should.be.within(1, 100)
-        body.should.deep.contain({
-          ticker: 'lakebtc',
-          buy: '2700',
-          sell: '2690',
-          duration
-        })
-      }))
+      .then(({ body }) => expectTicker(body[1], {
+        ticker: 'lakebtc',
+        buy: '2700',
+        sell: '2690'
+      })))
   })
 
   describe('Coinfloor api', () => {
     it('response with 200 and coinfloor prices', () => getPriceData()
       .expect(200)
-      .then(({ body }) => {
-        const duration = body[2].duration
-        duration.should.be.within(1, 100)
-        body.should.deep.contain({
-          ticker: 'coinfloor',
-          buy: '3554',
-          sell: '3545',
-          duration
-        })
-      }))
+      .then(({ body }) => expectTicker(body[2], {
+        ticker: 'coinfloor',
+        buy: '3554',
+        sell: '3545'
+      }, coinfloorDelayMs)))
   })
 })
