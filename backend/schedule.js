8ba2e54ce36a0c4816192bb42e6ecc0841a1ec86
mikemey <miguel.maestro@gmx.co.uk>
2017-08-30 21:13:57 +0100

    fixed scheduler timing

--- backend/schedule.js
+++ backend/schedule.js
@@ -8,5 +8,5 @@ mongoConnection.init(config, log)
   .then(() => {
     log.info('setting up scheduler...')
     const tickerScheduleService = require('./schedule/tickerScheduleService')(log)
-    schedule.scheduleJob('*/10 * * * * *', tickerScheduleService.storeTickers)
+    schedule.scheduleJob('*/1 * * * *', tickerScheduleService.storeTickers)
   })
