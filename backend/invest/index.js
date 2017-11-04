649d0df784c2b8958f8f5bdd75b2757f20a9c656
mikemey <miguel.maestro@gmx.co.uk>
2017-11-04 21:19:36 +0000

    added transaction charts

--- backend/invest/index.js
+++ backend/invest/index.js
@@ -10,13 +10,15 @@ const createInvestRouter = logger => {
 
   const transactionCache = {
     cutoffTimestamp: 0,
-    data: []
+    transactionList: [],
+    latestPrice: 0,
+    priceGroups: []
   }
   let txidCache = []
 
   const filterExistingTransactions = txs => txs
     .filter(tx => !txidCache.includes(tx.tid))
-  const mergeWithLatestTransactions = txs => txs.concat(transactionCache.data)
+  const mergeWithLatestTransactions = txs => txs.concat(transactionCache.transactionList)
   const sortTransactions = txs => txs.sort((a, b) => b.tid - a.tid)
 
   const cutoffTimestamp = () => moment.utc().subtract(10, 'm').unix()
@@ -24,19 +26,55 @@ const createInvestRouter = logger => {
     const cutoff = cutoffTimestamp()
     return {
       cutoff,
-      data: txs.filter(tx => tx.date > cutoff)
+      transactionList: txs.filter(tx => tx.date > cutoff),
+      latestPrice: Number(txs[0].price)
     }
   }
 
+  const calculateWeightedAmount = filteredTxs => {
+    const timeScope = moment.utc().unix() - filteredTxs.cutoff
+    filteredTxs.transactionList.forEach(tx => {
+      tx.weightedAmount = (tx.amount * (tx.date - filteredTxs.cutoff) / timeScope)
+    })
+    return filteredTxs
+  }
+
+  const groupByPrices = (priceGroups, tx) => {
+    const txPrice = Number(tx.price)
+    const priceGroup = priceGroups.find(group => group.label === txPrice)
+
+    if (priceGroup) {
+      priceGroup.weighted += tx.weightedAmount
+    } else {
+      priceGroups.push({
+        label: txPrice,
+        weighted: tx.weightedAmount
+      })
+    }
+    return priceGroups
+  }
+
+  const createPriceGroups = filteredTxs => {
+    filteredTxs.priceGroups = filteredTxs.transactionList
+      .reduce(groupByPrices, [])
+      .sort((groupA, groupB) => groupA.label - groupB.label)
+    return filteredTxs
+  }
+
   const updateTransactionCaches = filteredTxs => {
-    console.log(`transactions previous: ${transactionCache.data.length} \tnew: ${filteredTxs.data.length}`)
+    logger.info('transactions previous: %s \tnew: %s',
+      transactionCache.transactionList.length,
+      filteredTxs.transactionList.length
+    )
     transactionCache.cutoffTimestamp = filteredTxs.cutoff
-    transactionCache.data = filteredTxs.data
-    txidCache = filteredTxs.data.map(tx => tx.tid)
+    transactionCache.transactionList = filteredTxs.transactionList
+    transactionCache.latestPrice = filteredTxs.latestPrice
+    transactionCache.priceGroups = filteredTxs.priceGroups
+    txidCache = filteredTxs.transactionList.map(tx => tx.tid)
   }
 
   const errorHandler = err => {
-    console.error(err.message)
+    logger.error(err.message)
   }
 
   const updateLatestTransactions = () => requests
@@ -45,10 +83,12 @@ const createInvestRouter = logger => {
     .then(mergeWithLatestTransactions)
     .then(sortTransactions)
     .then(removeOutdatedTransactions)
+    .then(calculateWeightedAmount)
+    .then(createPriceGroups)
     .then(updateTransactionCaches)
     .catch(errorHandler)
 
-  schedule.scheduleJob('10 * * * * *', updateLatestTransactions)
+  schedule.scheduleJob('*/5 * * * * *', updateLatestTransactions)
 
   router.get('/transactions', (req, res) =>
     res.status(200).json(transactionCache)
--- frontend/coinfloor/coinfloor-invest.js
+++ frontend/coinfloor/coinfloor-invest.js
@@ -1,4 +1,4 @@
-/* global angular */
+/* global angular Chart moment */
 
 const investControllerName = 'CoinfloorInvestController'
 
@@ -10,7 +10,62 @@ angular
   })
   .controller(investControllerName, ['$scope', '$interval', '$http',
     function ($scope, $interval, $http) {
+      const priceChart = new Chart('priceChart', {
+        type: 'horizontalBar',
+        data: {
+          labels: [],
+          datasets: [{
+            data: []
+          }]
+        },
+        options: {
+          responsive: true,
+          scales: {
+            xAxes: [{
+              barPercentage: 1.0,
+              categoryPercentage: 1.0
+            }],
+            yAxes: [{
+              position: 'right',
+              ticks: {
+                fontSize: 11,
+                fontFamily: 'Courier, monospace',
+                fontColor: '#000'
+              }
+            }]
+          }
+        }
+      })
+
+      const transactionChart = new Chart('transactionChart', {
+        type: 'line',
+        data: { labels: [], datasets: [] },
+        options: {
+          scales: {
+            xAxes: [{
+              type: 'time',
+              time: {
+                displayFormats: {
+                  minute: 'H:mm'
+                }
+              }
+            }],
+            yAxes: [{
+              position: 'right',
+              ticks: {
+                fontFamily: 'Courier, monospace',
+                fontColor: '#000'
+              }
+            }]
+          }
+        }
+      })
+
       $scope.model = {
+        priceChart,
+        priceChartData: priceChart.data,
+        transactionChart,
+        transactionChartData: transactionChart.data,
         latestTransactions: [],
         dateLimit: 0
       }
@@ -22,10 +77,47 @@ angular
           return []
         })
 
+      const fillPriceGaps = priceGroups => {
+        const start = priceGroups[0].label
+        const end = priceGroups[priceGroups.length - 1].label
+        const priceRange = Array.from({ length: end - start + 1 }, (_, k) => k + start)
+
+        return priceRange.map(price => {
+          const priceGroup = priceGroups.find(group => group.label === price)
+          return priceGroup || { label: price, weighted: 0 }
+        })
+      }
+
       const updateTransactionsChart = () => requestTransactions()
         .then(txs => {
           $scope.model.dateLimit = txs.cutoffTimestamp
-          $scope.model.latestTransactions = txs.data
+          $scope.model.latestTransactions = txs.transactionList
+
+          const filledPriceGroups = fillPriceGaps(txs.priceGroups).reverse()
+          $scope.model.priceChartData.labels = filledPriceGroups.map(group => group.label)
+          $scope.model.priceChartData.datasets = [{
+            data: filledPriceGroups.map(group => group.weighted),
+            borderWidth: 0,
+            backgroundColor: filledPriceGroups.map(group =>
+              group.label === txs.latestPrice
+                ? 'rgba(255,120,120,0.5)'
+                : 'rgba(151,187,255,0.7)'
+            )
+          }]
+          $scope.model.priceChart.update(0)
+
+          $scope.model.transactionChartData.datasets = [{
+            data: txs.transactionList.map(tx => {
+              const x = moment.unix(tx.date)
+              const y = tx.price
+              return { x, y }
+            }),
+            fill: false,
+            borderColor: 'rgb(151,187,255)',
+            backgroundColor: 'rgb(151,187,255)',
+            borderWidth: 4
+          }]
+          $scope.model.transactionChart.update(0)
         })
 
       $scope.stop = $interval(updateTransactionsChart, 1500)
