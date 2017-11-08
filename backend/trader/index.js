24e6cf12089d5c5c39b1d8b6862ef1966b93e615
mikemey <miguel.maestro@gmx.co.uk>
2017-11-08 22:14:20 +0000

    added trader scheduling

---
+++ backend/trader/index.js
@@ -0,0 +1,36 @@
+const schedule = require('node-schedule')
+const moment = require('moment')
+
+const timeStamped = message => {
+  const ts = moment().format('YYYY-MM-DD HH:mm:ss')
+  return `[${ts}] ${message}`
+}
+
+const logger = {
+  info: message => console.log(timeStamped(message)),
+  error: message => console.error(timeStamped(message))
+}
+
+logger.info('setting up trader...')
+
+const { traderConfig } = require('./config')
+
+const exchangeConnector = require('./exchangeConnector')(traderConfig)
+const surgeDetector = require('./surgeDetector')(logger, traderConfig, exchangeConnector)
+const openOrdersWatch = require('./openOrdersWatch')(logger, traderConfig, exchangeConnector)
+const orderIssuer = require('./orderIssuer')(logger, traderConfig, openOrdersWatch, exchangeConnector)
+
+const errorHandler = err => {
+  logger.error(err.message)
+  job.cancel()
+}
+
+const tick = () => Promise.all([
+  surgeDetector.analyseTrends(),
+  openOrdersWatch.resolveOpenOrders()
+]).then(orderIssuer.issueOrders)
+  .catch(errorHandler)
+
+const job = schedule.scheduleJob('10,30,50 * * * * *', tick)
+
+logger.info('trader running')
