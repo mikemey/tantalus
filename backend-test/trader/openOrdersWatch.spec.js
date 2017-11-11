1dbe2f097f47b9e4f24c351dcc7b3dbdcbb91cf3
mikemey <miguel.maestro@gmx.co.uk>
2017-11-11 23:57:34 +0000

    open orders: removed account creation (finished using only exchange account)

--- backend-test/trader/openOrdersWatch.spec.js
+++ backend-test/trader/openOrdersWatch.spec.js
@@ -44,30 +44,13 @@ describe('Open orders watch', () => {
   const buyOrder = (id, amount, price) => openOrder(id, 0, amount, price)
   const sellOrder = (id, amount, price) => openOrder(id, 1, amount, price)
 
-  const resolveOpenOrders = (exchangeOpenOrders, expectedAvailableAmount, expectedAvailableVolume) => {
+  const resolveOpenOrders = exchangeOpenOrders => {
     const getOrderScope = expectGetOpenOrders(exchangeOpenOrders)
     return openOrdersWatch.resolveOpenOrders()
-      .then(result => {
-        getOrderScope.isDone().should.equal(true, 'getOrderScope')
-        result.availableAmount.should.equal(expectedAvailableAmount, 'availableAmount')
-        result.availableVolume.should.equal(expectedAvailableVolume, 'availableVolume')
-      })
+      .then(() => getOrderScope.isDone().should.equal(true, 'getOrderScope'))
   }
 
   describe('orders safety checks', () => {
-    it('throws error when volume in buy order is higher than available', () => {
-      expect(openOrdersWatch.addOpenOrder.bind(openOrdersWatch, buyOrder(123, 2001, 500000)))
-        .to.throw(Error, 'buying with more volume than available: £ 1000.50 > £ 1000.00')
-    })
-
-    it('throws error when amount in sell order is larger than available', () => {
-      openOrdersWatch.addOpenOrder(buyOrder(444, 2000, 500000))
-      return resolveOpenOrders([], 2000, 0).then(() => {
-        expect(openOrdersWatch.addOpenOrder.bind(openOrdersWatch, sellOrder(555, 2001, 500000)))
-          .to.throw(Error, 'selling more btcs than available: Ƀ 0.2001 > Ƀ 0.2000')
-      })
-    })
-
     it('throw error when local order type not recognised', () => {
       expect(openOrdersWatch.addOpenOrder.bind(openOrdersWatch, openOrder(99, 3, 100, 200000)))
         .to.throw(Error, 'unknown local order type: 3')
@@ -81,111 +64,87 @@ describe('Open orders watch', () => {
   })
 
   describe('common scenarios', () => {
-    it('should return full volume and no amount when nothing happens', () => {
-      return resolveOpenOrders([], 0, testVolumeLimit)
+    it('should do nothing when starting', () => {
+      return resolveOpenOrders([])
     })
 
     it('full cycle with partial amounts', () => {
       openOrdersWatch.addOpenOrder(buyOrder(100, 2000, 500000))
       expectCancelOrder(100)
 
-      return resolveOpenOrders([buyOrder(100, 199, 500000)], 1801, 0)
+      return resolveOpenOrders([buyOrder(100, 199, 500000)])
         .then(() => {
           openOrdersWatch.addOpenOrder(sellOrder(102, 1801, 500500))
           expectCancelOrder(102)
-          return resolveOpenOrders([sellOrder(102, 1700, 500500)], 1700, 15005)
+          return resolveOpenOrders([sellOrder(102, 1700, 500500)])
         })
         .then(() => {
           openOrdersWatch.addOpenOrder(buyOrder(104, 298, 500800))
           openOrdersWatch.addOpenOrder(sellOrder(106, 1700, 501100))
-          return resolveOpenOrders([], 298, 85269)
+          return resolveOpenOrders([])
         })
-        .then(() => resolveOpenOrders([], 298, 85269))
+        .then(() => resolveOpenOrders([]))
         .then(() => {
           openOrdersWatch.addOpenOrder(sellOrder(108, 298, 501500))
-          return resolveOpenOrders([], 0, testVolumeLimit)
+          return resolveOpenOrders([])
         })
     })
   })
 
-  describe('volume -> amount', () => {
-    it('fully when order bought fully', () => {
+  describe('BUY order', () => {
+    it('NOT cancelled when completely bought', () => {
       openOrdersWatch.addOpenOrder(buyOrder(123, 2000, 500000))
-      return resolveOpenOrders([], 2000, 0)
+      return resolveOpenOrders([])
     })
 
-    it('revert when order still open', () => {
+    it('cancels order when still open', () => {
       const order = buyOrder(540, 1500, 500000)
       openOrdersWatch.addOpenOrder(order)
-      expectCancelOrder(540)
-      return resolveOpenOrders([order], 0, 100000)
+      const cancelScope = expectCancelOrder(540)
+      return resolveOpenOrders([order])
+        .then(() => cancelScope.isDone().should.equal(true))
     })
 
-    it('partially when order partially bought', () => {
+    it('cancels order when partially bought', () => {
       openOrdersWatch.addOpenOrder(buyOrder(124, 1980, 505000))
       const cancelScope = expectCancelOrder(124)
-
-      return resolveOpenOrders([buyOrder(124, 481, 505000)], 1499, 24300)
+      return resolveOpenOrders([buyOrder(124, 481, 505000)])
         .then(() => cancelScope.isDone().should.equal(true))
     })
 
-    it('fully when order partially bought and cancel order failed', () => {
+    it('ignores cancel order failed', () => {
       openOrdersWatch.addOpenOrder(buyOrder(125, 2000, 500000))
       const cancelScope = expectCancelOrder(125, false)
-
-      return resolveOpenOrders([buyOrder(125, 500, 500000)], 2000, 0)
-        .then(() => cancelScope.isDone().should.equal(true))
-    })
-
-    it('none when order partially bought remaining volume under lower limit', () => {
-      openOrdersWatch.addOpenOrder(buyOrder(128, 2000, 500000))
-      const cancelScope = expectCancelOrder(128)
-
-      const amountUnderLimit = 199
-      return resolveOpenOrders([buyOrder(128, amountUnderLimit, 500000)], 1801, 0)
+      return resolveOpenOrders([buyOrder(125, 500, 500000)])
         .then(() => cancelScope.isDone().should.equal(true))
     })
   })
 
-  describe('amount -> volume', () => {
-    beforeEach(() => {
-      openOrdersWatch.addOpenOrder(buyOrder(444, 2000, 500000))
-      return resolveOpenOrders([], 2000, 0)
-    })
-
-    it('fully when amount sold', () => {
+  describe('SELL order', () => {
+    it('NOT cancelled when completely sold', () => {
       openOrdersWatch.addOpenOrder(sellOrder(555, 2000, 500000))
-      return resolveOpenOrders([], 0, 100000)
+      return resolveOpenOrders([])
     })
 
-    it('revert when order still open', () => {
+    it('cancels order when still open', () => {
       const order = sellOrder(222, 1500, 550000)
       openOrdersWatch.addOpenOrder(order)
-      expectCancelOrder(222)
-      return resolveOpenOrders([order], 2000, 0)
+      const cancelScope = expectCancelOrder(222)
+      return resolveOpenOrders([order])
+        .then(() => cancelScope.isDone().should.equal(true))
     })
 
-    it('partially when part of amount sold', () => {
+    it('cancels order when partially sold', () => {
       openOrdersWatch.addOpenOrder(sellOrder(666, 2000, 550100))
       const cancelScope = expectCancelOrder(666)
-
-      return resolveOpenOrders([sellOrder(666, 1507, 550100)], 1507, 27119)
+      return resolveOpenOrders([sellOrder(666, 1507, 550100)])
         .then(() => cancelScope.isDone().should.equal(true))
     })
 
-    it('fully when part of amount sold and cancel order failed', () => {
+    it('ignores cancel order failed', () => {
       openOrdersWatch.addOpenOrder(sellOrder(777, 2000, 500000))
       const cancelScope = expectCancelOrder(777, false)
-
-      return resolveOpenOrders([sellOrder(777, 500000, 100)], 0, testVolumeLimit)
-        .then(() => cancelScope.isDone().should.equal(true))
-    })
-
-    it('maximum amount when larger volume sold', () => {
-      openOrdersWatch.addOpenOrder(sellOrder(888, 2000, 600000))
-      const cancelScope = expectCancelOrder(888)
-
-      return resolveOpenOrders([sellOrder(888, 100, 550000)], 100, testVolumeLimit)
+      return resolveOpenOrders([sellOrder(777, 500000, 100)])
         .then(() => cancelScope.isDone().should.equal(true))
     })
   })
--- backend/trader/openOrdersWatch.js
+++ backend/trader/openOrdersWatch.js
@@ -1,6 +1,4 @@
 const {
-  amountString,
-  volumeString,
   floorVolume,
   createOrderLogger,
   isBuyOrder,
@@ -9,119 +7,63 @@ const {
 
 const OpenOrdersWatch = (baseLogger, config, exchangeConnector) => {
   const orderLogger = createOrderLogger(baseLogger)
-  const volumeLimit = config.buying.volumeLimitPence
-  const lowerLimit = config.buying.lowerLimitPence
-
-  const accounts = {
-    availableVolume: volumeLimit,
-    availableAmount: 0
-  }
-
   const localOpenOrders = new Map()
 
   const addOpenOrder = newLocalOrder => {
-    if (newLocalOrder === undefined) orderLogger.error('OOW.addOpenOrder.newLocalOrder')
     newLocalOrder.volume = floorVolume(newLocalOrder.amount, newLocalOrder.price)
 
-    checkLocalOrder(newLocalOrder)
+    checkOrderType(newLocalOrder)
     localOpenOrders.set(newLocalOrder.id, newLocalOrder)
     orderLogger.logNewOrder(newLocalOrder)
-
-    if (isBuyOrder(newLocalOrder)) accounts.availableVolume -= newLocalOrder.volume
-    if (isSellOrder(newLocalOrder)) accounts.availableAmount -= newLocalOrder.amount
   }
 
-  const checkLocalOrder = newLocalOrder => {
-    if (isBuyOrder(newLocalOrder)) {
-      if (newLocalOrder.volume > accounts.availableVolume) {
-        const newVolume = volumeString(newLocalOrder.volume)
-        const availableVolume = volumeString(accounts.availableVolume)
-        throw new Error(`buying with more volume than available: ${newVolume} > ${availableVolume}`)
-      }
-    } else if (isSellOrder(newLocalOrder)) {
-      if (newLocalOrder.amount > accounts.availableAmount) {
-        const newAmount = amountString(newLocalOrder.amount)
-        const availableAmount = amountString(accounts.availableAmount)
-        throw new Error(`selling more btcs than available: ${newAmount} > ${availableAmount}`)
-      }
-    } else {
-      throw new Error(`unknown local order type: ${newLocalOrder.type}`)
+  const checkOrderType = (newOrder, origin = 'local') => {
+    if (!isBuyOrder(newOrder) && !isSellOrder(newOrder)) {
+      throw new Error(`unknown ${origin} order type: ${newOrder.type}`)
     }
   }
 
   const checkExchangeOrders = exchangeOrders => {
-    exchangeOrders.forEach(order => {
-      if (!isBuyOrder(order) && !isSellOrder(order)) {
-        throw new Error(`unknown exchange order type: ${order.type}`)
-      }
-    })
+    exchangeOrders.forEach(order => checkOrderType(order, 'exchange'))
     return exchangeOrders
   }
 
   const cancelUnresolvedOrder = exchangeOrders => Promise.all(exchangeOrders.map(exchangeOrder =>
     exchangeConnector.cancelOrder(exchangeOrder.id)
       .then(cancelSuccess => {
-        const localOrder = localOpenOrders.get(exchangeOrder.id)
         if (cancelSuccess) {
+          const localOrder = localOpenOrders.get(exchangeOrder.id)
           localOpenOrders.delete(exchangeOrder.id)
 
-          if (isBuyOrder(exchangeOrder)) cancelBuyOrder(localOrder, exchangeOrder)
-          if (isSellOrder(exchangeOrder)) cancelSellOrder(localOrder, exchangeOrder)
+          logPartiallyBoughtSoldOrder(localOrder, exchangeOrder)
         }
       })
   ))
 
-  const cancelBuyOrder = (localOrder, exchangeOrder) => {
-    const amountBought = localOrder.amount - exchangeOrder.amount
-    if (amountBought > 0) {
-      if (localOrder === undefined) orderLogger.error('OOW.cancelBuyOrder.localOrder')
-      orderLogger.logOrderBought(localOrder.id, amountBought, localOrder.price)
-      accounts.availableAmount += amountBought
-    }
-    if (exchangeOrder === undefined) orderLogger.error('OOW.cancelBuyOrder.exchangeOrder')
-    accounts.availableVolume += floorVolume(exchangeOrder.amount, exchangeOrder.price)
+  const logPartiallyBoughtSoldOrder = (localOrder, exchangeOrder) => {
+    const amountDiff = localOrder.amount - exchangeOrder.amount
+    const logFunc = isBuyOrder(exchangeOrder)
+      ? orderLogger.logOrderBought
+      : orderLogger.logOrderSold
+    if (amountDiff > 0) logFunc(localOrder.id, amountDiff, localOrder.price)
   }
 
-  const cancelSellOrder = (localOrder, exchangeOrder) => {
-    const amountSold = localOrder.amount - exchangeOrder.amount
-    if (amountSold > 0) {
-      if (localOrder === undefined) orderLogger.error('OOW.cancelSellOrder.localOrder')
-      orderLogger.logOrderSold(localOrder.id, amountSold, localOrder.price)
-    }
-    accounts.availableAmount += exchangeOrder.amount
-    if (localOrder === undefined) orderLogger.error('OOW.cancelSellOrder.localOrder')
-    accounts.availableVolume += floorVolume(localOrder.amount - exchangeOrder.amount, localOrder.price)
-  }
-
-  const checkBoughtSoldOrder = () => {
+  const logFullyBoughtSoldOrder = () => {
     localOpenOrders.forEach(localOrder => {
-      if (localOrder === undefined) orderLogger.error('OOW.checkBoughtSoldOrder.localOrder')
       if (isBuyOrder(localOrder) && localOrder.amount > 0) {
         orderLogger.logOrderBought(localOrder.id, localOrder.amount, localOrder.price)
-        accounts.availableAmount += localOrder.amount
       }
       if (isSellOrder(localOrder) && localOrder.amount > 0) {
         orderLogger.logOrderSold(localOrder.id, localOrder.amount, localOrder.price)
-        accounts.availableVolume += localOrder.volume
       }
     })
     localOpenOrders.clear()
   }
 
-  const createAvailableAccounts = () => {
-    const availableAmount = accounts.availableAmount
-    const availableVolume = accounts.availableVolume < lowerLimit
-      ? 0
-      : Math.min(accounts.availableVolume, volumeLimit)
-
-    return { availableAmount, availableVolume }
-  }
-
   const resolveOpenOrders = () => exchangeConnector.getOpenOrders()
     .then(checkExchangeOrders)
     .then(cancelUnresolvedOrder)
-    .then(checkBoughtSoldOrder)
-    .then(createAvailableAccounts)
+    .then(logFullyBoughtSoldOrder)
 
   return {
     addOpenOrder,
