0bce6b722e2825c9f51e87668b06cfca0dc72e88
mikemey <miguel.maestro@gmx.co.uk>
2017-11-11 21:24:37 +0000

    trader config: extracted common parameters

--- backend/trader/config.js
+++ backend/trader/config.js
@@ -1,13 +1,12 @@
+const deepAssign = require('assign-deep')
+
 const traderConfigs = [{ // =========================== T 200
   clientId: 'T(200) B(15.0 / 3) S(- 0.6 / 2)',
-  exchangeHost: 'http://localhost:8000/api/simex',
   timeslotSeconds: 200,
   tickSchedule: '2-58/17 * * * * *',
   buying: {
     ratio: 15.0, // price change (£/Ƀ) per timeslotSeconds
-    useTimeslots: 3,
-    volumeLimitPence: 100000,
-    lowerLimitPence: 5000
+    useTimeslots: 3
   },
   selling: {
     ratio: -0.6, // price change (£/Ƀ) per timeslotSeconds
@@ -15,14 +14,11 @@ const traderConfigs = [{ // =========================== T 200
   }
 }, {
   clientId: 'T(200) B(12.5 / 3) S(- 0.1 / 2)',
-  exchangeHost: 'http://localhost:8000/api/simex',
   timeslotSeconds: 200,
   tickSchedule: '2-59/17 * * * * *',
   buying: {
     ratio: 12.5, // price change (£/Ƀ) per timeslotSeconds
-    useTimeslots: 3,
-    volumeLimitPence: 100000,
-    lowerLimitPence: 5000
+    useTimeslots: 3
   },
   selling: {
     ratio: -0.1, // price change (£/Ƀ) per timeslotSeconds
@@ -30,14 +26,11 @@ const traderConfigs = [{ // =========================== T 200
   }
 }, {
   clientId: 'T(200) B(10.0 / 3) S(  0.0 / 2)',
-  exchangeHost: 'http://localhost:8000/api/simex',
   timeslotSeconds: 200,
   tickSchedule: '2-59/17 * * * * *',
   buying: {
     ratio: 10.0, // price change (£/Ƀ) per timeslotSeconds
-    useTimeslots: 3,
-    volumeLimitPence: 100000,
-    lowerLimitPence: 5000
+    useTimeslots: 3
   },
   selling: {
     ratio: 0, // price change (£/Ƀ) per timeslotSeconds
@@ -45,14 +38,11 @@ const traderConfigs = [{ // =========================== T 200
   }
 }, {
   clientId: 'T(200) B( 7.0 / 3) S(- 0.1 / 2)',
-  exchangeHost: 'http://localhost:8000/api/simex',
   timeslotSeconds: 200,
   tickSchedule: '3-59/17 * * * * *',
   buying: {
     ratio: 7.0, // price change (£/Ƀ) per timeslotSeconds
-    useTimeslots: 3,
-    volumeLimitPence: 100000,
-    lowerLimitPence: 5000
+    useTimeslots: 3
   },
   selling: {
     ratio: -0.1, // price change (£/Ƀ) per timeslotSeconds
@@ -60,14 +50,11 @@ const traderConfigs = [{ // =========================== T 200
   }
 }, { // ================================================ T 150
   clientId: 'T(150) B( 7.0 / 4) S(- 0.1 / 2)',
-  exchangeHost: 'http://localhost:8000/api/simex',
   timeslotSeconds: 150,
   tickSchedule: '4-59/17 * * * * *',
   buying: {
     ratio: 7.0, // price change (£/Ƀ) per timeslotSeconds
-    useTimeslots: 4,
-    volumeLimitPence: 100000,
-    lowerLimitPence: 5000
+    useTimeslots: 4
   },
   selling: {
     ratio: -0.1, // price change (£/Ƀ) per timeslotSeconds
@@ -75,14 +62,11 @@ const traderConfigs = [{ // =========================== T 200
   }
 }, {
   clientId: 'T(150) B( 5.0 / 4) S(- 0.1 / 2)',
-  exchangeHost: 'http://localhost:8000/api/simex',
   timeslotSeconds: 150,
   tickSchedule: '4-59/17 * * * * *',
   buying: {
     ratio: 5.0, // price change (£/Ƀ) per timeslotSeconds
-    useTimeslots: 4,
-    volumeLimitPence: 100000,
-    lowerLimitPence: 5000
+    useTimeslots: 4
   },
   selling: {
     ratio: -0.1, // price change (£/Ƀ) per timeslotSeconds
@@ -90,14 +74,11 @@ const traderConfigs = [{ // =========================== T 200
   }
 }, {
   clientId: 'T(150) B( 3.0 / 4) S(  0.0 / 2)',
-  exchangeHost: 'http://localhost:8000/api/simex',
   timeslotSeconds: 150,
   tickSchedule: '5-59/17 * * * * *',
   buying: {
     ratio: 3.0, // price change (£/Ƀ) per timeslotSeconds
-    useTimeslots: 4,
-    volumeLimitPence: 100000,
-    lowerLimitPence: 5000
+    useTimeslots: 4
   },
   selling: {
     ratio: 0, // price change (£/Ƀ) per timeslotSeconds
@@ -105,14 +86,11 @@ const traderConfigs = [{ // =========================== T 200
   }
 }, { // ================================================ T 50
   clientId: 'T( 50) B( 5.0 /10) S(- 0.5 / 2)',
-  exchangeHost: 'http://localhost:8000/api/simex',
   timeslotSeconds: 50,
   tickSchedule: '5-59/17 * * * * *',
   buying: {
     ratio: 5.0, // price change (£/Ƀ) per timeslotSeconds
-    useTimeslots: 10,
-    volumeLimitPence: 100000,
-    lowerLimitPence: 5000
+    useTimeslots: 10
   },
   selling: {
     ratio: -0.5, // price change (£/Ƀ) per timeslotSeconds
@@ -120,14 +98,11 @@ const traderConfigs = [{ // =========================== T 200
   }
 }, {
   clientId: 'T( 50) B( 3.0 /10) S(- 0.5 / 2)',
-  exchangeHost: 'http://localhost:8000/api/simex',
   timeslotSeconds: 50,
   tickSchedule: '6-59/17 * * * * *',
   buying: {
     ratio: 3.0, // price change (£/Ƀ) per timeslotSeconds
-    useTimeslots: 10,
-    volumeLimitPence: 100000,
-    lowerLimitPence: 5000
+    useTimeslots: 10
   },
   selling: {
     ratio: -0.5, // price change (£/Ƀ) per timeslotSeconds
@@ -135,14 +110,11 @@ const traderConfigs = [{ // =========================== T 200
   }
 }, { // ================================================ T 300
   clientId: 'T(300) B(10.0 / 3) S(- 0.3 / 2)',
-  exchangeHost: 'http://localhost:8000/api/simex',
   timeslotSeconds: 300,
   tickSchedule: '6-59/17 * * * * *',
   buying: {
     ratio: 10.0, // price change (£/Ƀ) per timeslotSeconds
-    useTimeslots: 3,
-    volumeLimitPence: 100000,
-    lowerLimitPence: 5000
+    useTimeslots: 3
   },
   selling: {
     ratio: -0.3, // price change (£/Ƀ) per timeslotSeconds
@@ -150,14 +122,11 @@ const traderConfigs = [{ // =========================== T 200
   }
 }, {
   clientId: 'T(300) B( 7.0 / 3) S(- 0.3 / 2)',
-  exchangeHost: 'http://localhost:8000/api/simex',
   timeslotSeconds: 300,
   tickSchedule: '7-59/17 * * * * *',
   buying: {
     ratio: 7.0, // price change (£/Ƀ) per timeslotSeconds
-    useTimeslots: 3,
-    volumeLimitPence: 100000,
-    lowerLimitPence: 5000
+    useTimeslots: 3
   },
   selling: {
     ratio: -0.3, // price change (£/Ƀ) per timeslotSeconds
@@ -165,14 +134,11 @@ const traderConfigs = [{ // =========================== T 200
   }
 }, {
   clientId: 'T(300) B( 5.0 / 3) S(- 0.3 / 2)',
-  exchangeHost: 'http://localhost:8000/api/simex',
   timeslotSeconds: 300,
   tickSchedule: '8-59/17 * * * * *',
   buying: {
     ratio: 5.0, // price change (£/Ƀ) per timeslotSeconds
-    useTimeslots: 3,
-    volumeLimitPence: 100000,
-    lowerLimitPence: 5000
+    useTimeslots: 3
   },
   selling: {
     ratio: -0.3, // price change (£/Ƀ) per timeslotSeconds
@@ -180,14 +146,11 @@ const traderConfigs = [{ // =========================== T 200
   }
 }, {
   clientId: 'T(300) B( 3.5 / 3) S(- 0.1 / 2)',
-  exchangeHost: 'http://localhost:8000/api/simex',
   timeslotSeconds: 300,
   tickSchedule: '9-59/17 * * * * *',
   buying: {
     ratio: 3.5, // price change (£/Ƀ) per timeslotSeconds
-    useTimeslots: 3,
-    volumeLimitPence: 100000,
-    lowerLimitPence: 5000
+    useTimeslots: 3
   },
   selling: {
     ratio: -0.1, // price change (£/Ƀ) per timeslotSeconds
@@ -195,14 +158,11 @@ const traderConfigs = [{ // =========================== T 200
   }
 }, {
   clientId: 'T(300) B( 7.0 / 2) S(- 1.0 / 1)',
-  exchangeHost: 'http://localhost:8000/api/simex',
   timeslotSeconds: 300,
   tickSchedule: '3-58/9 * * * * *',
   buying: {
     ratio: 7.0, // price change (£/Ƀ) per timeslotSeconds
-    useTimeslots: 2,
-    volumeLimitPence: 100000,
-    lowerLimitPence: 5000
+    useTimeslots: 2
   },
   selling: {
     ratio: -0.50, // price change (£/Ƀ) per timeslotSeconds
@@ -210,14 +170,11 @@ const traderConfigs = [{ // =========================== T 200
   }
 }, {
   clientId: 'T(200) B( 5.0 / 2) S(- 0.1 / 1)',
-  exchangeHost: 'http://localhost:8000/api/simex',
   timeslotSeconds: 200,
   tickSchedule: '5-59/9 * * * * *',
   buying: {
     ratio: 5.0, // price change (£/Ƀ) per timeslotSeconds
-    useTimeslots: 2,
-    volumeLimitPence: 100000,
-    lowerLimitPence: 5000
+    useTimeslots: 2
   },
   selling: {
     ratio: -0.1, // price change (£/Ƀ) per timeslotSeconds
@@ -225,14 +182,11 @@ const traderConfigs = [{ // =========================== T 200
   }
 }, {
   clientId: 'T(200) B( 7.0 / 2) S(- 0.1 / 1)',
-  exchangeHost: 'http://localhost:8000/api/simex',
   timeslotSeconds: 200,
   tickSchedule: '7-59/9 * * * * *',
   buying: {
     ratio: 7.0, // price change (£/Ƀ) per timeslotSeconds
-    useTimeslots: 2,
-    volumeLimitPence: 100000,
-    lowerLimitPence: 5000
+    useTimeslots: 2
   },
   selling: {
     ratio: -0.1, // price change (£/Ƀ) per timeslotSeconds
@@ -240,20 +194,25 @@ const traderConfigs = [{ // =========================== T 200
   }
 }, {
   clientId: 'T(200) B(10.0 / 2) S(- 0.1 / 1)',
-  exchangeHost: 'http://localhost:8000/api/simex',
   timeslotSeconds: 200,
   tickSchedule: '9-59/9 * * * * *',
   buying: {
     ratio: 10.0, // price change (£/Ƀ) per timeslotSeconds
-    useTimeslots: 2,
-    volumeLimitPence: 100000,
-    lowerLimitPence: 5000
+    useTimeslots: 2
   },
   selling: {
     ratio: -0.1, // price change (£/Ƀ) per timeslotSeconds
     useTimeslots: 1
   }
 }]
+
+const commonConfig = {
+  exchangeHost: 'http://localhost:8000/api/simex',
+  buying: {
+    volumeLimitPence: 100000,
+    lowerLimitPence: 5000
+  }
+}
 
 const checkForDuplicates = () => traderConfigs.reduce((existing, config) => {
   if (existing.includes(config.clientId)) {
@@ -265,7 +224,7 @@ const checkForDuplicates = () => traderConfigs.reduce((existing, config) => {
 
 const getTraderConfigs = () => {
   checkForDuplicates()
-  return traderConfigs
+  return traderConfigs.map(config => deepAssign(config, commonConfig))
 }
 
 module.exports = { getTraderConfigs }
