2b7fbefc5428fe18881f62e004dc568f47c211c4
mikemey <miguel.maestro@gmx.co.uk>
2017-11-12 00:21:29 +0000

    trader: added balance log on start

--- backend/trader/traderInstance.js
+++ backend/trader/traderInstance.js
@@ -1,6 +1,11 @@
 const schedule = require('node-schedule')
 
-const { createOrderLogger, createClientLogger } = require('../utils/ordersHelper')
+const {
+  amountString,
+  volumeString,
+  createOrderLogger,
+  createClientLogger
+ } = require('../utils/ordersHelper')
 
 const createTrader = (baseLogger, config) => {
   const clientLogger = createClientLogger(baseLogger, config.clientId)
@@ -10,6 +15,12 @@ const createTrader = (baseLogger, config) => {
   const surgeDetector = require('./surgeDetector')(clientLogger, config, exchangeConnector)
   const openOrdersWatch = require('./openOrdersWatch')(clientLogger, config, exchangeConnector)
   const orderIssuer = require('./orderIssuer')(clientLogger, config, openOrdersWatch, exchangeConnector)
+
+  exchangeConnector.getAccount().then(account => {
+    const amount = account.balances.xbt_available
+    const volume = account.balances.gbp_available
+    traderLogger.info(`Balance: ${volumeString(volume)} - ${amountString(amount)}`)
+  })
 
   const errorHandler = (err, issueStop = true) => {
     traderLogger.error(err.message)
