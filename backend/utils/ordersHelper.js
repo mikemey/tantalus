63f92929e6329eb660f5349ea7ccdbf7828d9207
mikemey <miguel.maestro@gmx.co.uk>
2017-11-15 20:26:18 +0000

    Logging: fixed and normalized BLO/SLO/BOUGHT/SOLD

--- backend/utils/ordersHelper.js
+++ backend/utils/ordersHelper.js
@@ -8,10 +8,10 @@ const {
 const mmBTC = 10000
 
 const amountString = amount => `Ƀ ${(amount / mmBTC).toFixed(4)}`
-const priceString = (price, unit = '£/Ƀ') => `${unit} ${(price / 100).toFixed(2)}`
+const priceString = price => `£/Ƀ ${(price / 100).toFixed(0)}`
 const amountPriceString = (amount, price) => `${amountString(amount)} ${priceString(price)}`
 
-const volumeString = volume => priceString(volume, '£')
+const volumeString = volume => `£ ${(volume / 100).toFixed(2)}`
 
 const floorVolume = (amount, price) => Math.floor(amount * price / mmBTC)
 const reverseVolume = (volume, amountOrPrice) => Math.floor(volume / amountOrPrice * mmBTC)
@@ -19,8 +19,8 @@ const floorAmount = reverseVolume
 const floorPrice = reverseVolume
 
 const orderName = order => order.type === BUY_ORDER_TYPE ? 'BO' : 'SO'
-const BOUGHT_MSG = 'B'
-const SOLD_MSG = 'S'
+const BOUGHT_MSG = 'BT'
+const SOLD_MSG = 'SD'
 
 // order type: (0 - buy; 1 - sell)
 const BUY_ORDER_TYPE = 0
@@ -30,19 +30,19 @@ const isSellOrder = order => order.type === SELL_ORDER_TYPE
 
 const OrderLogger = baseLogger => {
   const logNewOrder = order => baseLogger.info(darkText(
-    `${orderName(order)}[${order.id}] ${amountPriceString(order.amount, order.price)}`
+    `${orderName(order)} [${order.id}] ${amountPriceString(order.amount, order.price)}`
   ))
 
   const logCancelledOrder = order => baseLogger.info(
-    darkText(`${orderName(order)} [${order.id}]:`) + highlightText(' cancelled')
+    darkText(`${orderName(order)} [${order.id}]`) + highlightText(' cancelled')
   )
 
   const logOrderBought = (orderId, amount, price) => baseLogger.info(redText(
-    `${BOUGHT_MSG} [${orderId}]: ${amountPriceString(amount, price)}`
+    `${BOUGHT_MSG} [${orderId}] ${amountPriceString(amount, price)}`
   ))
 
   const logOrderSold = (orderId, amount, price) => baseLogger.info(greenText(
-    `${SOLD_MSG}[${orderId}] ${amountPriceString(amount, price)}`
+    `${SOLD_MSG} [${orderId}] ${amountPriceString(amount, price)}`
   ))
 
   return Object.assign({}, baseLogger, {
