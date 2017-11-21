868a385740418c0ce01bc4d07f40b00359ae4fff
mikemey <miguel.maestro@gmx.co.uk>
2017-11-21 15:42:46 +0000

    PartitionWorkerMock: fixed styling

--- backend-test/simrun/partitionWorkerMock.js
+++ backend-test/simrun/partitionWorkerMock.js
@@ -1,22 +1,22 @@
 const requests = require('../../backend/utils/requests')
 
 class PartitionWorkerMock {
-  constructor() {
+  constructor () {
     this.starIx = 0
     this.endIx = 0
     this.sendResults = (action, results) => requests
       .postJson(`http://localhost:12345/${action}`, results)
   }
 
-  createTraders(workerConfig) {
+  createTraders (workerConfig) {
     return this.sendResults('createTradersCalled', workerConfig)
   }
 
-  drainTransactions(transactionsSlice) {
+  drainTransactions (transactionsSlice) {
     return this.sendResults('drainTransactionsCalled', transactionsSlice)
   }
 
-  getAccounts() {
+  getAccounts () {
     return [
       { fullVolume: process.pid * 2 },
       { fullVolume: process.pid }
