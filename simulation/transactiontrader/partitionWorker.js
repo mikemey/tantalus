3a05c98597036cf5c51a9d708f3a6204ec3a8827
mikemey <miguel.maestro@gmx.co.uk>
2017-12-01 21:39:29 +0000

    Simprove: updated partition worker iteration run

--- simulation/transactiontrader/partitionWorker.js
+++ simulation/transactiontrader/partitionWorker.js
@@ -1,50 +1,75 @@
-const { TantalusLogger } = require('../utils/tantalusLogger')
-const { roundVolume } = require('../utils/ordersHelper')
+const mongo = require('../../utils/mongoConnection')
 
-const ExchangeAccountAdapter = require('./exchangeAccountAdapter')
-const TradeAccount = require('../backend/simex/tradeAccount')
-const TraderJob = require('../trader/traderJob')
+const { timestamp } = require('../simrunUtils')
+const { TantalusLogger } = require('../../utils/tantalusLogger')
+const { roundVolume } = require('../../utils/ordersHelper')
+const { executorConfig } = require('../simrunConfig')
+
+const TransactionRepo = require('../../transactions/transactionsRepo')
+const TransactionsSource = require('./txSource')
+const TransactionSlicer = require('./txSlicer')
 
 const baseLogger = console
 
-const quietLogger = {
-  errorOnly: true,
-  error: baseLogger.error,
-  log: baseLogger.log
-}
-
-const createSimulatedTrader = config => {
-  const tradeAccount = TradeAccount(quietLogger, config.clientId, config.buying.volumeLimitPence)
-  const exchangeAdapter = ExchangeAccountAdapter(tradeAccount)
-  const trader = TraderJob(quietLogger, config, exchangeAdapter)
-  return { trader, exchangeAdapter }
-}
-
 class PartitionWorker {
-  constructor (simulatedTraderFunc = createSimulatedTrader) {
+  // eslint-disable-next-line space-before-function-paren
+  constructor() {
     this.traderPairs = []
     this.lastTransactionPrice = 0
-    this.simulatedTraderFunc = simulatedTraderFunc
     this.logger = TantalusLogger(baseLogger, `worker-${process.pid}`)
   }
 
-  createTraders (workerConfigObject) {
-    this.lastTransactionPrice = 0
-    this.traderPairs = workerConfigObject.traderConfigs.map(this.simulatedTraderFunc)
-  }
-
-  drainTransactions (transactionsSlice) {
-    const txs = transactionsSlice.transactions
-    if (txs.length) {
-      this.lastTransactionPrice = txs[0].price
-      this.traderPairs.map(({ trader, exchangeAdapter }) => {
-        exchangeAdapter.setTransactions(txs)
-        trader.tick(transactionsSlice.unixNow)
-      })
+  // eslint-disable-next-line space-before-function-paren
+  errorHandler(prefix) {
+    return err => {
+      this.logger.error(prefix + err.message)
+      this.logger.log(err)
+      if (err.cause !== undefined) this.errorHandler('<=== CAUSED BY: ')(err.cause)
     }
   }
+  // eslint-disable-next-line space-before-function-paren
+  createTraders(workerConfigObject) {
+    return mongo.initializeDirectConnection(executorConfig, this.logger)
+      .then(() => {
+        this.txSlicer = TransactionSlicer(this.logger, workerConfigObject, executorConfig.transactionsUpdateSeconds)
+        this.lastTransactionPrice = 0
 
-  getAccounts () {
+        this.txsrc = TransactionsSource(this.logger, TransactionRepo())
+        return this.txsrc.reset(executorConfig.batchSeconds)
+      })
+      .catch(this.errorHandler('Partition worker (creating traders): '))
+  }
+
+  // eslint-disable-next-line space-before-function-paren
+  runIteration(iterationProgress) {
+    const itString = `[it-${iterationProgress}]`
+    const runIterationPromise = () => {
+      if (this.txsrc.hasNext()) {
+        this.logger.info('reading transactions...')
+        return this.txsrc.next()
+          .then(({ batchNum, from, to, transactions }) => {
+            const num = batchNum.toString().padStart(this.txsrc.batchCount().toString().length)
+            this.logger.info(`${itString} batch ` +
+              `[${num}/${this.txsrc.batchCount()}]: ${timestamp(from)} -> ${timestamp(to)}`
+            )
+            if (!this.txSlicer.isReady()) {
+              this.txSlicer.setStartDate(from)
+            }
+            this.lastTransactionPrice = transactions[transactions.length - 1].price
+            this.txSlicer.runBatch(transactions)
+            return runIterationPromise()
+          })
+      }
+      this.logger.info(`${itString} no more batches, draining last transactions...`)
+      this.txSlicer.drainLastSlice()
+      return Promise.resolve()
+    }
+
+    return runIterationPromise().catch(this.errorHandler('Partition worker (running iteration): '))
+  }
+
+  // eslint-disable-next-line space-before-function-paren
+  getAccounts() {
     return this.traderPairs.map(({ trader, exchangeAdapter }) => {
       const account = exchangeAdapter.getAccount()
       return {
