28914355f414289cb33b125bda69696d32757ad5
mikemey <miguel.maestro@gmx.co.uk>
2018-01-13 19:57:47 +0000

    Ticker-box: removed markets data

--- frontend/ticker/ticker-box.js
+++ frontend/ticker/ticker-box.js
@@ -7,24 +7,13 @@ angular.module('tantalus.ticker')
     controller: ticker,
     templateUrl: 'ticker/ticker-box.html'
   })
-  .controller(ticker, ['$scope', '$http', '$interval', 'tickerService', function ($scope, $http, $interval, tickerService) {
+  .controller(ticker, ['$scope', 'tickerService', function ($scope, tickerService) {
     $scope.model = { created: '', tickers: [] }
-    $scope.markets = []
-    $scope.marketDate = null
-
-    const setMarkets = () => $http.get('/api/markets')
-      .then(result => {
-        $scope.marketDate = new Date()
-        $scope.markets = result.data.sort((a, b) => b.trading.localeCompare(a.trading))
-      })
-      .catch(error => console.log(error))
 
     const setTicker = latestTicker => {
       $scope.model = latestTicker
     }
 
-    $interval(setMarkets, 60000)
     tickerService.getLatestTicker().then(setTicker)
     tickerService.watchTicker($scope, setTicker)
-    return setMarkets()
   }])
