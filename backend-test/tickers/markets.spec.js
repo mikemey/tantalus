636606cd2626ad53a174cb83fdc8a23ebbd811d9
mikemey <miguel.maestro@gmx.co.uk>
2018-02-08 13:49:42 +0000

    Backend-markets: added individual symbols to market query

--- backend-test/tickers/markets.spec.js
+++ backend-test/tickers/markets.spec.js
@@ -4,142 +4,38 @@ const nock = require('nock')
 
 const helpers = require('../../utils-test/helpers')
 
-const markets = [
-  { host: 'https://api.binance.com', path: '/api/v3/ticker/price?symbol=ETHBTC', response: '{"symbol":"ETHBTC","price":"0.06504100"}' },
-  { host: 'https://api.binance.com', path: '/api/v3/ticker/price?symbol=XRPBTC', response: '{"symbol":"XRPBTC","price":"0.00015849"}' },
-  { host: 'https://api.binance.com', path: '/api/v3/ticker/price?symbol=LSKBTC', response: '{"symbol":"LSKBTC","price":"0.0020804"}' },
-  { host: 'https://api.kraken.com', path: '/0/public/Ticker?pair=XXRPXXBT,XETHXXBT', response: '{"error":[],"result":{"XETHXXBT":{"a":["0.064530","1","1.000"],"b":["0.063800","24","24.000"],"c":["0.064530","0.03559574"],"v":["16855.85969725","31268.51313018"],"p":["0.062332","0.058423"],"t":[8353,16657],"l":["0.058270","0.057830"],"h":["0.064970","0.064970"],"o":"0.058380"},"XXRPXXBT":{"a":["0.000157940","82","82.000"],"b":["0.000157350","11531","11531.000"],"c":["0.000157950","62.55708645"],"v":["2482218.99869350","8320941.23009165"],"p":["0.000158911","0.000156253"],"t":[4436,12488],"l":["0.000153000","0.000152200"],"h":["0.000163800","0.000175520"],"o":"0.000154680"}}}' }
-]
-
-const TEST_HTML = '<!DOCTYPE html></html>'
-
 describe('GET /api/markets endpoint', () => {
   let app, server
+
+  const markets = [
+    { host: 'https://api.binance.com', path: '/api/v3/ticker/price?symbol=ETHBTC', response: '{"symbol":"ETHBTC","price":"0.08642600"}' },
+    { host: 'https://api.binance.com', path: '/api/v3/ticker/price?symbol=LTCBTC', response: '{"symbol":"LTCBTC","price":"0.01696400"}' },
+    { host: 'https://api.binance.com', path: '/api/v3/ticker/price?symbol=LSKBTC', response: '{"symbol":"LSKBTC","price":"0.0020804"}' }
+  ]
+  const mockMarketData = market => nock(market.host).get(market.path).reply(200, market.response)
 
   before(() => helpers.startTestServer((_app, _server) => {
     app = _app
     server = _server
   }))
+  beforeEach(() => markets.forEach(mockMarketData))
 
-  afterEach(() => nock.cleanAll())
   after(() => helpers.closeAll(server))
+  afterEach(() => nock.cleanAll())
 
-  const getMarketData = () => request(app).get('/api/markets')
+  const getBinanceMarketData = () => request(app).get('/api/markets/binance?symbols=ETHBTC,LTCBTC,LSKBTC')
 
-  const nockMarket = market => nock(market.host).get(market.path)
-  const mockMarketData = market => nockMarket(market).reply(200, market.response)
-
-  describe('valid single market responses', () => {
-    beforeEach(() => markets.forEach(mockMarketData))
-
-    it('should response with market data', () => {
-      const expectedMarketData = [
-        { name: 'Binance', trading: 'ETH/BTC', price: '0.06504100' },
-        { name: 'Binance', trading: 'XRP/BTC', price: '0.00015849' },
-        { name: 'Binance', trading: 'LSK/BTC', price: '0.0020804' },
-        { name: 'Kraken', trading: 'ETH/BTC', price: '0.064530' },
-        { name: 'Kraken', trading: 'XRP/BTC', price: '0.000157950' }
-      ]
-
-      return getMarketData().expect(200)
-        .then(({ body }) => body.should.deep.equal(expectedMarketData))
-    })
+  it('should response with available data', () => {
+    const expectedMarketData = [
+      { symbol: 'ETHBTC', price: 0.086426 },
+      { symbol: 'LTCBTC', price: 0.016964 },
+      { symbol: 'LSKBTC', price: 0.0020804 }
+    ]
+    return getBinanceMarketData().expect(200)
+      .then(({ body }) => { body.should.deep.equal(expectedMarketData) })
   })
 
-  describe('combined apis have failing responses', () => {
-    const goodMarkets = [markets[0], markets[1], markets[2]]
-    beforeEach(() => goodMarkets.forEach(mockMarketData))
-
-    it('should return markets when combined apis returns unknown data', () => {
-      nockMarket(markets[3]).twice().reply(200, TEST_HTML)
-      const expectedMarketData = [
-        { name: 'Binance', trading: 'ETH/BTC', price: '0.06504100' },
-        { name: 'Binance', trading: 'XRP/BTC', price: '0.00015849' },
-        { name: 'Binance', trading: 'LSK/BTC', price: '0.0020804' },
-        { name: 'Kraken', trading: 'ETH/BTC', price: 'N/A' },
-        { name: 'Kraken', trading: 'XRP/BTC', price: 'N/A' }
-      ]
-
-      return getMarketData().expect(200)
-        .then(({ body }) => body.should.deep.equal(expectedMarketData))
-    })
-
-    it('should return markets when combined apis returns 522', () => {
-      nockMarket(markets[3]).twice().reply(522, TEST_HTML)
-      const expectedMarketData = [
-        { name: 'Binance', trading: 'ETH/BTC', price: '0.06504100' },
-        { name: 'Binance', trading: 'XRP/BTC', price: '0.00015849' },
-        { name: 'Binance', trading: 'LSK/BTC', price: '0.0020804' },
-        { name: 'Kraken', trading: 'ETH/BTC', price: 'N/A' },
-        { name: 'Kraken', trading: 'XRP/BTC', price: 'N/A' }
-      ]
-
-      return getMarketData().expect(200)
-        .then(({ body }) => body.should.deep.equal(expectedMarketData))
-    })
-  })
-
-  describe('single api query has failing responses', () => {
-    const goodMarkets = [markets[0], markets[2], markets[3]]
-    beforeEach(() => goodMarkets.forEach(mockMarketData))
-
-    it('should return markets when single api query returns unknown data', () => {
-      nockMarket(markets[1]).twice().reply(200, TEST_HTML)
-      const expectedMarketData = [
-        { name: 'Binance', trading: 'ETH/BTC', price: '0.06504100' },
-        { name: 'Binance', trading: 'XRP/BTC', price: 'N/A' },
-        { name: 'Binance', trading: 'LSK/BTC', price: '0.0020804' },
-        { name: 'Kraken', trading: 'ETH/BTC', price: '0.064530' },
-        { name: 'Kraken', trading: 'XRP/BTC', price: '0.000157950' }
-      ]
-
-      return getMarketData().expect(200)
-        .then(({ body }) => body.should.deep.equal(expectedMarketData))
-    })
-
-    it('should return markets when single api query returns 522', () => {
-      nockMarket(markets[1]).twice().reply(522, markets[1].response)
-      const expectedMarketData = [
-        { name: 'Binance', trading: 'ETH/BTC', price: '0.06504100' },
-        { name: 'Binance', trading: 'XRP/BTC', price: 'N/A' },
-        { name: 'Binance', trading: 'LSK/BTC', price: '0.0020804' },
-        { name: 'Kraken', trading: 'ETH/BTC', price: '0.064530' },
-        { name: 'Kraken', trading: 'XRP/BTC', price: '0.000157950' }
-      ]
-
-      return getMarketData().expect(200)
-        .then(({ body }) => body.should.deep.equal(expectedMarketData))
-    })
-  })
-
-  describe('general market response', () => {
-    const getBinanceMarketData = () => request(app).get('/api/markets/binance')
-
-    it('should response with available data', () => {
-      mockMarketData({
-        host: 'https://api.binance.com',
-        path: '/api/v3/ticker/price',
-        response: '[{"symbol":"ETHBTC","price":"0.08642600"},{"symbol":"LTCBTC","price":"0.01696400"}]'
-      })
-
-      const expectedMarketData = [
-        { symbol: 'ETHBTC', price: 0.086426 },
-        { symbol: 'LTCBTC', price: 0.016964 }
-      ]
-      return getBinanceMarketData().expect(200)
-        .then(({ body }) => { body.should.deep.equal(expectedMarketData) })
-    })
-
-    it('should response with with empty list when server error', () => {
-      nockMarket({ host: 'https://api.binance.com', path: '/api/v3/ticker/price' }).twice().reply(522)
-
-      const expectedMarketData = []
-      return getBinanceMarketData().expect(200)
-        .then(({ body }) => { body.should.deep.equal(expectedMarketData) })
-    })
-
-    it('should response with error when unknown market requestes', () => {
-      return request(app).get('/api/markets/other').expect(404, 'market not supported: other')
-    })
+  it('should response with error when unknown market requestes', () => {
+    return request(app).get('/api/markets/other').expect(404, 'market not supported: other')
   })
 })
--- backend/tickers/markets.js
+++ backend/tickers/markets.js
@@ -2,87 +2,21 @@ const express = require('express')
 
 const requests = require('../../utils/requests')
 
-const KRAKEN = 'Kraken'
-const BINANCE = 'Binance'
+const binancePriceUrl = symbol => `https://api.binance.com/api/v3/ticker/price?symbol=${symbol}`
 
-const UNAVAILABLE = 'N/A'
-const ETH_BTC = 'ETH/BTC'
-const XRP_BTC = 'XRP/BTC'
-const LSK_BTC = 'LSK/BTC'
-
-const BINANCE_PRICE_URL = 'https://api.binance.com/api/v3/ticker/price'
-
-const binancePriceUrl = symbol => `${BINANCE_PRICE_URL}?symbol=${symbol}`
+const queryBinance = query => {
+  const symbols = query.split(',')
+  return Promise.all(symbols.map(symbol => requests.getJson(binancePriceUrl(symbol))))
+}
 const createMarketsRouter = logger => {
   const router = express.Router()
 
-  const extractBinancePrice = jsonResponse => jsonResponse.price
-    ? jsonResponse.price
-    : UNAVAILABLE
-
-  const singleMarketQueries = [
-    { name: BINANCE, trading: ETH_BTC, url: binancePriceUrl('ETHBTC'), price: extractBinancePrice },
-    { name: BINANCE, trading: XRP_BTC, url: binancePriceUrl('XRPBTC'), price: extractBinancePrice },
-    { name: BINANCE, trading: LSK_BTC, url: binancePriceUrl('LSKBTC'), price: extractBinancePrice }
-  ]
-
-  const marketResponse = (name, trading, price) => { return { name, trading, price } }
-
-  const errorHandler = query => err => {
-    logger.error(err.message)
-    return marketResponse(query.name, query.trading, UNAVAILABLE)
-  }
-
-  const requestKrakenData = () => requests
-    .getJson('https://api.kraken.com/0/public/Ticker?pair=XXRPXXBT,XETHXXBT')
-    .then(response => {
-      return [
-        extractFromKrakenResonse(response, 'XETHXXBT', ETH_BTC),
-        extractFromKrakenResonse(response, 'XXRPXXBT', XRP_BTC)
-      ]
-    })
-    .catch(error => {
-      logger.error(error.message)
-      return [
-        marketResponse(KRAKEN, ETH_BTC, UNAVAILABLE),
-        marketResponse(KRAKEN, XRP_BTC, UNAVAILABLE)]
-    })
-
-  const extractFromKrakenResonse = (response, symbol, trading) => {
-    const res = response.result
-    return res !== undefined && res[symbol] !== undefined && res[symbol].c[0] !== undefined
-      ? marketResponse(KRAKEN, trading, res[symbol].c[0])
-      : marketResponse(KRAKEN, trading, UNAVAILABLE)
-  }
-
-  const requestSingleQuery = query => {
-    return requests.getJson(query.url)
-      .then(result => {
-        return {
-          name: query.name,
-          trading: query.trading,
-          price: query.price(result)
-        }
-      })
-      .catch(errorHandler(query))
-  }
-
-  router.get('/', (req, res) => {
-    const requestPromises = singleMarketQueries.map(requestSingleQuery)
-      .concat(requestKrakenData())
-    return Promise.all(requestPromises)
-      .then(result => {
-        const merged = [].concat.apply([], result)
-        return res.json(merged)
-      })
-  })
-
   router.get('/:market', (req, res) => {
     if (req.params.market === 'binance') {
-      return requests.getJson(BINANCE_PRICE_URL)
-        .then(response => {
-          response.forEach(symbolPrice => { symbolPrice.price = Number(symbolPrice.price) })
-          res.json(response)
+      return queryBinance(req.query.symbols)
+        .then(responses => {
+          responses.forEach(symbolPrice => { symbolPrice.price = Number(symbolPrice.price) })
+          res.json(responses)
         })
         .catch(error => {
           logger.log(error)
