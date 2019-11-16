d43a892db9d1fae1cbf220fe64154b5acd753b1f
mikemey <miguel.maestro@gmx.co.uk>
2019-11-16 14:36:06 +0000

    add latest ticker to page title

--- frontend/ticker/ticker-box.js
+++ frontend/ticker/ticker-box.js
@@ -1,18 +1,25 @@
 /* global angular */
 
 const ticker = 'TickerController'
+const defaultTitle = 'Tantalus'
 
 angular.module('tantalus.ticker')
   .component('tickerBox', {
     controller: ticker,
     templateUrl: 'ticker/ticker-box.html'
   })
-  .controller(ticker, ['$scope', 'tickerService', function ($scope, tickerService) {
+  .controller(ticker, ['$scope', 'tickerService', '$document', function ($scope, tickerService, $document) {
     $scope.model = { created: '', tickers: [] }
 
+    const setTitle = title => { $document[0].title = title }
+
     const setTicker = latestTicker => {
+      const titleTicker = latestTicker.tickers.find(ticker => ticker.name === 'coinfloor')
+      if (titleTicker) setTitle(`${defaultTitle} [£ ${titleTicker.bid} / £ ${titleTicker.ask}]`)
       $scope.model = latestTicker
     }
+
+    $scope.$on('$destroy', () => setTitle(defaultTitle))
 
     tickerService.getLatestTicker().then(setTicker)
     tickerService.watchTicker($scope, setTicker)
