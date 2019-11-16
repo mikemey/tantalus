52e87bbc00c777d38af8ddaa0f72c3ea70197763
mikemey <miguel.maestro@gmx.co.uk>
2019-11-16 16:18:18 +0000

    add optional links for known tickers

--- frontend/ticker/ticker-box.js
+++ frontend/ticker/ticker-box.js
@@ -3,6 +3,12 @@
 const ticker = 'TickerController'
 const defaultTitle = 'Tantalus'
 
+const CF_NAME = 'coinfloor'
+const tickerLinks = [
+  { name: CF_NAME, linkto: 'https://coinfloor.co.uk/exchange' },
+  { name: 'gdax', linkto: 'https://pro.coinbase.com/trade/BTC-GBP' }
+]
+
 angular.module('tantalus.ticker')
   .component('tickerBox', {
     controller: ticker,
@@ -13,10 +19,20 @@ angular.module('tantalus.ticker')
 
     const setTitle = title => { $document[0].title = title }
 
-    const setTicker = latestTicker => {
-      const titleTicker = latestTicker.tickers.find(ticker => ticker.name === 'coinfloor')
+    const setTicker = latestTickerData => {
+      const titleTicker = latestTickerData.tickers.find(ticker => ticker.name === CF_NAME)
       if (titleTicker) setTitle(`[£ ${titleTicker.bid} /£ ${titleTicker.ask}] ${defaultTitle}`)
-      $scope.model = latestTicker
+
+      $scope.model = updateTickerLinks(latestTickerData)
+    }
+
+    const updateTickerLinks = tickerData => {
+      tickerData.tickers = tickerData.tickers.map(ticker => {
+        const link = tickerLinks.find(link => ticker.name === link.name)
+        if (link) ticker.linkto = link.linkto
+        return ticker
+      })
+      return tickerData
     }
 
     $scope.$on('$destroy', () => setTitle(defaultTitle))
