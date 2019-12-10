917068ba47aa138f67b93823c846278ebc763d67
mikemey <miguel.maestro@gmx.co.uk>
2019-12-10 12:50:22 +0000

    add script to import live db into local db

---
+++ import_tantalus_livedb.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+DUMP_FILE=live_dump
+
+echo "dumping remote tickers..."
+ssh $MSMSERVER /bin/bash << EOF
+  mongodump --db=tantalus --excludeCollection=accounts --excludeCollection=sessions --archive=$DUMP_FILE
+EOF
+
+echo "transferring dump file..."
+rsync --progress --partial -r $MSMSERVER:~/$DUMP_FILE .
+
+echo "importing dump file..."
+mongorestore --drop --archive=$DUMP_FILE
+
+echo "cleanup..."
+rm $DUMP_FILE
+ssh $MSMSERVER /bin/bash << EOF
+  rm $DUMP_FILE
+EOF
+
+echo "done"
